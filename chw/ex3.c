d0d819627e7debbd5e652f5d70a38ea8e2dbf1fe
petegq <petegq@gmail.com>
2021-06-08 09:26:08 -0400

    Break it - remove a var

--- chw/ex3.c
+++ chw/ex3.c
@@ -5,7 +5,9 @@ int main()
 	int age = 10;
 	int height = 72;
 	
-	printf("I am %d years old. \n", age);
+	printf("I am %d years old. \n"); 
+	// Remove 'age' = warning: format ‘%d’ expects a matching ‘int’ argument
+	// Also > 'I am -702442952 years old' on running it.
 	printf("I am %d inches tall. \n", height);
 	
 	return 0;
