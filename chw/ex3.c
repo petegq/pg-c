c69d2c6b5ef332da5307a09257429ce9b4068ab8
petegq <petegq@gmail.com>
2021-06-08 09:29:40 -0400

    Break it - uninitialised var

--- chw/ex3.c
+++ chw/ex3.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-	int age = 10;
+	int age;
+	// Removing initial value from var = warning: ‘age’ is used uninitialized in this function
+	// Also > 'I am 0 years old' on execution.
 	int height = 72;
 	
-	printf("I am %d years old. \n"); 
+	printf("I am %d years old. \n", age); 
 	// Remove 'age' = warning: format ‘%d’ expects a matching ‘int’ argument
-	// Also > 'I am -702442952 years old' on running it.
+	// Also > 'I am -702442952 years old' on execution.
 	printf("I am %d inches tall. \n", height);
 	
 	return 0;
