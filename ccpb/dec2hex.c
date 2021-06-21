c2040b267a07552a426902a943c556b49cd8afcf
Peter G <petegq@gmail.com>
2021-06-21 12:42:32 +0100

    BG ex1: decimal to hex converter

---
+++ ccpb/dec2hex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("===^^===\n");
+    printf("Welcome to the Decimal to Hexadecimal Converter!\n");
+    printf("Enter a number:\n");
+
+    int the_number; // declares integer var
+
+    scanf("%d", &the_number); // acquire user input
+
+    printf("Decimal representation:		%9d\n", the_number); // %d number as decimal
+    printf("Converted to hexadecimal:	%9X\n", the_number); // %x number in hexadecimal
+    // + 9 or other number to use space for that many digits
+
+    printf("===^^===\n");
+
+    return 0;
+}
