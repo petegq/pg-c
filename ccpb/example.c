796d5e4d1980a807d18b385fab9cdeee107c71f7
Peter G <petegq@gmail.com>
2021-06-21 17:01:02 +0100

    Errors and warning, number types int long

--- ccpb/example.c
+++ ccpb/example.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    // int number = 100000000000000;    // = warning: implicit conversion from 'long' to 'int' changes value from 100000000000000 to 276447232 [-Wconstant-conversion]
+    long number = 100000000000000;
+
+    // printf("%d \n", number);     // int num... printf("%d", number) for 276447232
+    printf("%ld \n", number); // long num... printf("%ld... for expected output of 100000000000000
+        // %d = decimal
+        // %ld = long decimal
+
+    printf("Hello world! \n");
+
     return EXIT_SUCCESS;
 }
