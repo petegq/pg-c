d5e96fdfad241fdfe90a80c511853406834c4054
Peter Garside-Quin <petegq@gmail.com>
2022-03-23 19:53:37 +0000

    ex21

---
+++ ccpb/ex21.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const int girls = 12;
+    printf("girls: %d\n", girls);
+
+    // girls = 15; // cannot reassign ofc.
+
+    return EXIT_SUCCESS;
+}
